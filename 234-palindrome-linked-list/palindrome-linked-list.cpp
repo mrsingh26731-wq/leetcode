41bf7a930458f91d08887acf379d73a047798a56
Nitin <mr.singh26731@gmail.com>
2026-08-19 12:30:12 +0530

    Time: 4 ms (48.55%) | Memory: 114.1 MB (98.96%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,30 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-
-        stack<int> st;
-
-        ListNode* temp = head;
-
-        // Push all elements into stack
-        while (temp != nullptr) {
-            st.push(temp->val);
-            temp = temp->next;
+        ListNode*slow=head;
+        ListNode*fast=head;
+        ListNode*prev=nullptr;
+        ListNode*next=nullptr;
+        while(fast!=nullptr and fast->next!=nullptr){
+            fast=fast->next->next;
+            next=slow->next;
+            slow->next = prev;
+            prev=slow;
+            slow=next;
         }
-
-        // Compare
-        temp = head;
-
-        while (temp != nullptr) {
-
-            if (temp->val != st.top()) {
+        if(fast!=nullptr){
+            slow=slow->next;
+        }
+        while(slow!=nullptr and prev!=nullptr){
+            if(slow->val!=prev->val){
                 return false;
             }
-
-            st.pop();
-            temp = temp->next;
+            slow=slow->next;
+            prev=prev->next;
         }
-
-        return true;
+        return true;        
     }
 };
